1879cec47ba146b46c530ba77fa48454702f2d51
Kaung-Htet-Hein <kaunghtethein.dev@gmail.com>
2025-06-12 22:39:37 +0800

    feat: added cookie token handle

--- api/middleware/auth_middleware.go
+++ api/middleware/auth_middleware.go
@@ -3,8 +3,6 @@ package middleware
 import (
 	"KaungHtetHein116/IVY-backend/api/transport"
 	"KaungHtetHein116/IVY-backend/pkg/constants"
-	"encoding/json"
-	"log"
 	"net/http"
 	"strings"
 
@@ -67,6 +65,10 @@ var openRoutes = []paths{
 		path:   "/api/v1/booking/:id",
 		method: http.MethodGet,
 	},
+	{
+		path:   "/api/v1/user/clerk-user-webhook",
+		method: http.MethodPost,
+	},
 }
 
 func RegisterAuthMiddleware(e *echo.Echo) {
@@ -76,11 +78,24 @@ func RegisterAuthMiddleware(e *echo.Echo) {
 func JWTMiddleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+
 			if !shouldAuthorize(c) {
 				return next(c)
 			}
 
-			sessionToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+			var sessionToken string
+
+			tokenCookie, err := c.Cookie("__session")
+
+			if err != nil && tokenCookie.Value != "" {
+				sessionToken = tokenCookie.Value
+			} else if authHeader := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer "); authHeader != "" {
+				sessionToken = authHeader
+			}
+
+			if sessionToken == "" {
+				return transport.NewApiErrorResponse(c, http.StatusUnauthorized, constants.ErrUnauthorized, nil)
+			}
 
 			claims, err := jwt.Verify(c.Request().Context(), &jwt.VerifyParams{
 				Token: sessionToken,
@@ -98,16 +113,14 @@ func JWTMiddleware() echo.MiddlewareFunc {
 
 			c.Set("user_id", usr.ID)
 
-			userJson, err := json.Marshal(usr)
-			if err != nil {
-				log.Printf("Failed to marshal user: %v", err)
-			} else {
-				log.Printf("User ID: %s, User: %s", usr.ID, userJson)
-			}
+			// userJson, err := json.Marshal(usr)
+			// if err != nil {
+			// 	log.Printf("Failed to marshal user: %v", err)
+			// } else {
+			// 	log.Printf("User ID: %s, User: %s", usr.ID, userJson)
+			// }
 
 			// // Try to get token from cookie first
-			// cookie, err := c.Cookie("__session")
-			// var tokenString string
 
 			// if err == nil {
 			// 	// Token found in cookie
@@ -139,14 +152,14 @@ func JWTMiddleware() echo.MiddlewareFunc {
 }
 
 func shouldAuthorize(c echo.Context) bool {
-	// requestPath := c.Path()
-	// requestMethod := c.Request().Method
+	requestPath := c.Path()
+	requestMethod := c.Request().Method
 
-	// // Check if the current route is in excluded routes
-	// for _, route := range openRoutes {
-	// 	if route.path == requestPath && route.method == requestMethod {
-	// 		return false
-	// 	}
-	// }
+	// Check if the current route is in excluded routes
+	for _, route := range openRoutes {
+		if route.path == requestPath && route.method == requestMethod {
+			return false
+		}
+	}
 	return false
 }
